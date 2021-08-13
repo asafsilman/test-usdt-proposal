9f300907526fe1b61f58e913f6d0f2d67ba7ebe7
Andrea Franz <andrea@gravityblast.com>
2021-08-13 16:43:32 +0200

    fix iip-12

--- scripts/iip-12.ts
+++ scripts/iip-12.ts
@@ -47,15 +47,14 @@ export default task("iip-12", "Deploy IIP 11 to Disable AAVE v1", async(_, hre) 
     govTokensEqualLength // _newGovTokensEqualLen
   ]);
 
+  const priceOracle = await hre.ethers.getContractAt(PRICE_ORACLE_ABI, addresses.priceOracleV1);
+  const raiEthPriceFeed = "0x4ad7B025127e89263242aB68F0f9c4E5C033B489";
+  proposalBuilder = proposalBuilder.addContractAction(priceOracle, "updateFeedETH", [addresses.RAI.live, raiEthPriceFeed]);
 
   const idleController = await hre.ethers.getContractAt(IDLE_CONTROLLER_ABI, addresses.idleController);
   const idleRAISpeedBefore = await idleController.idleSpeeds(addresses.idleRAIV4);
   proposalBuilder = proposalBuilder.addContractAction(idleController, "_supportMarkets", [[idleRAI.address]]);
   proposalBuilder = proposalBuilder.addContractAction(idleController, "_addIdleMarkets", [[idleRAI.address]]);
-
-  const priceOracle = await hre.ethers.getContractAt(PRICE_ORACLE_ABI, addresses.priceOracleV2.live);
-  const raiEthPriceFeed = "0x4ad7B025127e89263242aB68F0f9c4E5C033B489";
-  proposalBuilder = proposalBuilder.addContractAction(priceOracle, "updateFeedETH", [addresses.RAI.live, raiEthPriceFeed]);
 
   const feeCollector = await hre.ethers.getContractAt(FEE_COLLECTOR_ABI, addresses.feeCollector)
   proposalBuilder.addContractAction(feeCollector, "registerTokenToDepositList", [addresses.RAI.live])
@@ -63,12 +62,34 @@ export default task("iip-12", "Deploy IIP 11 to Disable AAVE v1", async(_, hre) 
   proposalBuilder.setDescription("IIP-12 TODO");
   const proposal = proposalBuilder.build()
   await proposal.printProposalInfo();
-  await proposal.simulate();
-  console.log("Proposal simulated");
+
 
   const isLocalNet = hre.network.name == 'hardhat';
+
+  if (isLocalNet) {
+    console.log("Simulating proposal")
+    const WHALE_ADDRESS = addresses.devLeagueMultisig;
+    await hre.network.provider.send("hardhat_impersonateAccount", [WHALE_ADDRESS]);
+    let signer = await hre.ethers.getSigner(WHALE_ADDRESS);
+    await hre.network.provider.send("hardhat_setBalance", [WHALE_ADDRESS, "0xffffffffffffffff"]);
+    proposal.setProposer(signer);
+    // To run full simulation, set the flag for simulate to `true`
+    await proposal.simulate();
+    console.log("Proposal simulated :)");
+    console.log();
+  } else {
+    console.log('Posting proposal on-chain with Dev League Multisig');
+    const ledgerSigner = new LedgerSigner(hre.ethers.provider, undefined, "m/44'/60'/0'/0/0");
+    const service = new SafeService('https://safe-transaction.gnosis.io/');
+    const signer = await SafeEthersSigner.create(addresses.devLeagueMultisig, ledgerSigner, service, hre.ethers.provider);
+    proposal.setProposer(signer);
+    await proposal.propose();
+    console.log("Proposal is live");
+  }
+
+  console.log("--------------------------------------------------------")
+  // Skip tests in mainnet
   if (!isLocalNet) {
-    console.log("DONE");
     return;
   }
 
@@ -102,20 +123,29 @@ export default task("iip-12", "Deploy IIP 11 to Disable AAVE v1", async(_, hre) 
   if (!idleRAISpeedAfter.gt(toBN("0"))) {
     console.log("🚨🚨 ERROR!!! IdleRAI speed after proposal didn't increase");
   } else {
-    console.log("✅ Verified that IdleRAI speed increased after proposal")
+    console.log("✅ Verified that IdleRAI speed increased after proposal");
   }
 
   // RAI added as deposit tokens in Fee Collector
   const despositTokens = (await feeCollector.getDepositTokens()).map((a: string) => a.toLowerCase());
   if (despositTokens.includes(addresses.RAI.live.toLowerCase())) {
-    console.log("✅ Verified that RAI is enabled in feeCollector")
+    console.log("✅ Verified that RAI is enabled in feeCollector");
   } else {
-    console.log("🚨🚨 ERROR!!! Fee collector did not enable RAIs")
+    console.log("🚨🚨 ERROR!!! Fee collector did not enable RAIs");
   }
 
+  // IDLE added as gov token
+  const newGovTokens = (await idleRAI.getGovTokens()).map((a: string) => a.toLowerCase());
+  if (newGovTokens.includes(addresses.IDLE.toLowerCase())) {
+    console.log("✅ Verified that IDLE is added as gov token");
+  } else {
+    console.log("🚨🚨 ERROR!!! IdleRAI doens't have IDLE as gov token");
+  }
+
+  // Test Idle Token
   const allocationsSpread = currentProtocolTokens.map(() => parseInt((100000 / currentProtocolTokens.length).toFixed(0)))
   const diff = 100000 - allocationsSpread.reduce((p, c) => p + c); // check for rounding errors
   allocationsSpread[0] = allocationsSpread[0] + diff;
-  console.log('allocationsSpread', allocationsSpread.map(a => a.toString()))
+  console.log('allocationsSpread', allocationsSpread.map(a => a.toString()));
   // await hre.run("test-idle-token", {idleToken: idleRAI, allocations: allocationsSpread, unlent: 0, whale: ''})
 });
