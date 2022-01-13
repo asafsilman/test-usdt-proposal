e93d92a41445267d01a902baa8e759accb62c51b
dantop114 <dantop114@protonmail.com>
2022-01-13 16:10:18 +0100

    Adds transfers from Fee Treasury Fund and Ecosystem Fund

--- scripts/iip-18.ts
+++ scripts/iip-18.ts
@@ -5,6 +5,7 @@ import { AlphaProposalBuilder } from "@idle-finance/hardhat-proposals-plugin/dis
 const addresses = require("../common/addresses")
 const TimelockABI = require("../abi/Timelock.json");
 const ERC20_ABI = require("../abi/ERC20.json")
+const GovernableFundABI = require("../abi/GovernableFund.json");
 const GovernorAlphaABI = require("../abi/GovernorAlpha.json");
 const FeeCollectorABI = require("../abi/FeeCollector.json")
 const GovernorBravoDelegateABI = require("../abi/GovernorBravoDelegate.json");
@@ -23,6 +24,8 @@ export default task("iip-18", "Upgrade Governor Alpha")
 
         const timelock = await hre.ethers.getContractAt(TimelockABI, addresses.timelock);
         const feeCollector = await hre.ethers.getContractAt(FeeCollectorABI, addresses.feeCollector);
+        const feeTreasury = await hre.ethers.getContractAt(GovernableFundABI, addresses.feeTreasury);
+        const ecosystemFund = await hre.ethers.getContractAt(GovernableFundABI, addresses.ecosystemFund);
         const bpoolToken = await hre.ethers.getContractAt(ERC20_ABI, '0x859e4d219e83204a2ea389dac11048cc880b6aa8'); // idle smart treasury balancer pool token
 
         let governorBravo = await hre.ethers.getContractAt(GovernorBravoDelegateABI, governorBravoAddress);
@@ -31,7 +34,18 @@ export default task("iip-18", "Upgrade Governor Alpha")
         let feeCollectorBpoolBalance = await bpoolToken.balanceOf(addresses.feeCollector);
         let newAllocations = [toBN(0), toBN(20000), toBN(30000), toBN(50000)]
 
+        const idleToken = await hre.ethers.getContractAt(ERC20_ABI, addresses.IDLE); // idle token
+        const wethToken = await hre.ethers.getContractAt(ERC20_ABI, addresses.WETH['live']); // weth token
+        const usdcToken = await hre.ethers.getContractAt(ERC20_ABI, addresses.USDC['live']); // usdc token
+
+        const usdcBalanceFeeTreasury = usdcToken.balanceOf(addresses.feeTreasury);
+        const wethBalanceFeeTreasury = wethToken.balanceOf(addresses.feeTreasury);
+        const idleAmountToTransfer = toBN(48493).mul(ONE);
+
         proposalBuilder = proposalBuilder
+            .addContractAction(feeTreasury, "transfer", [addresses.USDC['live'], addresses.treasuryMultisig, usdcBalanceFeeTreasury])
+            .addContractAction(feeTreasury, "transfer", [addresses.WETH['live'], addresses.treasuryMultisig, wethBalanceFeeTreasury])
+            .addContractAction(ecosystemFund, "transfer", [addresses.IDLE, addresses.treasuryMultisig, idleAmountToTransfer])
             .addContractAction(feeCollector, "setSplitAllocation", [newAllocations])
             .addContractAction(feeCollector, "withdrawUnderlying", [addresses.treasuryMultisig, feeCollectorBpoolBalance, [0, 0]])
             .addContractAction(timelock, "setPendingAdmin", [governorBravoAddress])
@@ -43,10 +57,9 @@ export default task("iip-18", "Upgrade Governor Alpha")
         const proposal = proposalBuilder.build()
         await proposal.printProposalInfo();
 
-        const idleToken = await hre.ethers.getContractAt(ERC20_ABI, addresses.IDLE); // idle token
-        const wethToken = await hre.ethers.getContractAt(ERC20_ABI, addresses.WETH['live']); // weth token
         const treasuryIdleBalanceBefore = await idleToken.balanceOf(addresses.treasuryMultisig);
         const treasuryWETHBalanceBefore = await wethToken.balanceOf(addresses.treasuryMultisig);
+        const treasuryUSDCBalanceBefore = await usdcToken.balanceOf(addresses.treasuryMultisig);
 
         await hre.run('execute-proposal-or-simulate', { proposal, isLocalNet });
 
@@ -61,8 +74,11 @@ export default task("iip-18", "Upgrade Governor Alpha")
         const allocations = await feeCollector.getSplitAllocation();
         const treasuryIdleBalanceAfter = await idleToken.balanceOf(addresses.treasuryMultisig);
         const treasuryWETHBalanceAfter = await wethToken.balanceOf(addresses.treasuryMultisig);
+        const treasuryUSDCBalanceAfter = await usdcToken.balanceOf(addresses.treasuryMultisig);
+
         const treasuryIdleBalanceIncrease = treasuryIdleBalanceAfter.sub(treasuryIdleBalanceBefore);
         const treasuryWETHBalanceIncrease = treasuryWETHBalanceAfter.sub(treasuryWETHBalanceBefore);
+        const treasuryUSDCBalanceIncrease = treasuryUSDCBalanceAfter.sub(treasuryUSDCBalanceBefore);
 
         // bpool is going to get drained and treasury is going to receive more WETH of those in the pool
         // approximatively 208000 IDLEs and ~15.3 WETH (bpool is currently holding ~208161 IDLE and ~15.2 WETH)
@@ -70,7 +86,9 @@ export default task("iip-18", "Upgrade Governor Alpha")
         console.log(`Treasury IDLE balance increase: ${hre.ethers.utils.formatEther(treasuryIdleBalanceIncrease)}`);
         console.log(`Treasury WETH balance increase: ${hre.ethers.utils.formatEther(treasuryWETHBalanceIncrease)}`);
 
-        if(treasuryIdleBalanceIncrease.gt(toBN(208000).mul(ONE)) && treasuryWETHBalanceIncrease.gt(toBN(15).mul(ONE))) {
+        if (treasuryIdleBalanceIncrease.gt(toBN(208000).mul(ONE).add(idleAmountToTransfer)) 
+            && treasuryWETHBalanceIncrease.gt(toBN(15).mul(ONE).add(wethBalanceFeeTreasury))
+            && treasuryUSDCBalanceIncrease.eq(usdcBalanceFeeTreasury)) {
             console.log(`✅ Correct balance increases!`);
         } else {
             console.log('Incorrect increase in treasury balances');
